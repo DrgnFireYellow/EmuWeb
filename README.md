1d6c0ed5a9cfb65de0f625bfebd97a7bebceaade
DrgnFireYellow <ethanmilbert@gmail.com>
2023-12-08 09:49:09 -0500

    fix missing files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EmuWeb
 The Open Source Retro Emulation Frontend for the Web
+
+NOTE: At this time I am unable to create templates for platforms other than megadrive, n64, nes, and snes. If you would like to help add more templates, feel free to create a pull request.
+
+## Credits
+The template files are modified versions of files from the emulatorjs.org code editor.
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+SYSTEMS = ["nes", "snes", "n64", "megadrive"]
+indexcontents = "<ul>\n"
+
+if os.path.exists("output/games"):
+    shutil.rmtree("output/games")
+
+shutil.copytree("games", "output/games")
+if os.path.exists("output/artwork"):
+    shutil.rmtree("output/artwork")
+
+shutil.copytree("artwork", "output/artwork")
+
+def make_player(gamefile, system, htmlname):
+    with open(f"templates/{system}.html") as template:
+        player_content = template.read()
+    player_content = player_content.replace("$GAMEFILE", gamefile)
+    with open(f"output/{htmlname}.html", "w") as output:
+        output.write(player_content)
+
+for system in SYSTEMS:
+    for game in os.listdir(os.path.join("games", system)):
+        gamepath = os.path.join("games", system, game)
+        if game == "info.txt":
+            continue
+
+        if os.path.isfile(gamepath):
+            print(f"Creating page for {game}")
+            make_player(gamepath, system, game)
+            print(f"Checking for artwork for {game}")
+            artworkpath = os.path.join("artwork", f"{game}.png")
+            print(f"Adding {game} to index")
+            if os.path.isfile(artworkpath):
+                indexcontents += f"<li><a href=\"{game}.html\"><img src=\"{artworkpath}\"></a></li>\n"
+                continue
+            indexcontents += f"<li><a href=\"{game}.html\">{game}</a></li>\n"
+
+print("Creating index.html")
+indexcontents += "</ul>"
+with open("output/index.html", "w") as indexfile:
+    indexfile.write(indexcontents)
